5cd7a80653605723ee05ea162b1ced0e2aef562d
collo <qollomcbizzy64@gmail.com>
2017-08-02 13:03:16 +0300

    added changes to the app components and the styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class ="container">
+    <h1>My First Angular 2 App</h1>
+    <div class="row">
+      <div class="col-md-4 col-md-offset-2">
+        <h2><marquee direction="right">Welcome buddy !!</marquee></h2>
+      </div>
+    </div>
+  </div>
   `
 })
 //thic class holds the data and required documents
